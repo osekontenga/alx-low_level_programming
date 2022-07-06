9512f36773d5fc190c64c530a9d4e9f78d42b0f4
osekontenga <osekotrevor@gmail.com>
2022-07-06 09:12:36 -0700

    main.h - prototype

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  * main -prints to console
  *
  * Description: prints _putchar string to console
- *    
+ *
  * Return: Always 0 (Success)
  *
  */
@@ -15,10 +15,10 @@ int main(void)
 
 		while (text[i] != 0)
 		{
-							_putchar(text[i]);
-							i++;
+			 _putchar(text[i]);
+			i++;
 		}
-		_putchar('\n');
+		 _putchar('\n');
 
-						return (0);
+		return (0);
 }
