2301c4a77c0261701af8553406b0c2bc4a5c4ffb
osekontenga <osekotrevor@gmail.com>
2022-07-11 07:13:37 -0700

    function that takes a pointer to an int as parameter and updates the value it points to to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,17 +2,12 @@
 #include <stdio.h>
 
 /**
- * main - check the code 
+ * reset_to_98 - converts pointers to inegers
  *
  * Return: Always 0.
  */
-int main(void)
-{
-	int n;
 
-	n=402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
+void reset_to_98(int *n)
+{
+	n = (int*)n;
 }
